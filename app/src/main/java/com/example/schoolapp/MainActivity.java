e23cab9054f6ea874aa140c526bef8d004bd0810
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2022-01-25 02:07:36 +0100

    Add activities fragment

--- app/src/main/java/com/example/schoolapp/MainActivity.java
+++ app/src/main/java/com/example/schoolapp/MainActivity.java
@@ -7,14 +7,18 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toolbar;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private GoogleMap mMap;
     private DrawerLayout mDrawer;
     private NavigationView nvDrawer;
     private ActionBarDrawerToggle drawerToggle;
@@ -71,7 +75,10 @@ public class MainActivity extends AppCompatActivity {
                 fragmentClass = MainActivity.class;
                 break;
             case R.id.nav_second_fragment:
-                fragmentClass = MapsActivity.class;
+                fragmentClass = Maps.class;
+                break;
+            case R.id.nav_third_fragment:
+                fragmentClass = Activities.class;
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + menuItem.getItemId());
@@ -93,5 +100,15 @@ public class MainActivity extends AppCompatActivity {
         setTitle(menuItem.getTitle());
         // Close the navigation drawer
         mDrawer.closeDrawers();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
---
+++ app/src/main/java/com/example/schoolapp/Maps.java
@@ -0,0 +1,9 @@
+package com.example.schoolapp;
+
+import androidx.fragment.app.Fragment;
+
+public class Maps extends Fragment {
+    public Maps() {
+        super(R.layout.fragment_maps);
+    }
+}
